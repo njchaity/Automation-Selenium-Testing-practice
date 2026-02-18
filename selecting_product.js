45f55ac13adf33ce5734006ce38f13b160310e70
njchaity <nzchaity@gmail.com>
2026-02-18 15:29:55 +0600

    Adding the selecting product file

---
+++ selecting_product.js
@@ -0,0 +1,17 @@
+import { Builder, By, Key } from "selenium-webdriver";
+import * as chrome from "selenium-webdriver/chrome.js";
+
+async function testRunner() {
+  const driver = await new Builder().forBrowser("chrome").setChromeOptions(new chrome.Options()).build();
+
+  await driver.manage().window().maximize();
+  await driver.get("https://demo.evershop.io/");
+  await driver.findElement(By.xpath("//a[@class='search__icon']")).click();
+  await driver.findElement(By.xpath("//input[@placeholder='Search']")).sendKeys("Desk pen holder", Key.ENTER);
+  await driver.sleep(3000);
+  await driver.findElement(By.xpath("//a[@href='/accessories/desk-pen-holder-yellow']")).click();
+
+await driver.sleep(5000);
+  await driver.quit();
+}
+testRunner();
